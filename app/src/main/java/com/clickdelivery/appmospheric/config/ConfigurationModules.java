c0b123b0a61fa75662dd13330e500632b0b738d1
Antonio Jimenez <antonio-jimenez@accionplus.com>
2016-02-12 14:19:45 -0500

    First commit

---
+++ app/src/main/java/com/clickdelivery/appmospheric/config/ConfigurationModules.java
@@ -0,0 +1,17 @@
+package com.clickdelivery.appmospheric.config;
+
+import com.google.inject.AbstractModule;
+
+/**
+ * This is the RoboGuice configuration class
+ *
+ * @author <a href="mailto:aajn88@gmail.com">Antonio A. Jimenez N.</a>
+ */
+public class ConfigurationModules extends AbstractModule {
+
+    @Override
+    protected void configure() {
+
+    }
+
+}
