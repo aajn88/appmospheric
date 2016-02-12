d7630888b26d1f387fe9faaf17e7386341ddd8bf
Antonio Jimenez <antonio-jimenez@accionplus.com>
2016-02-12 14:37:52 -0500

    Splash screen added

--- app/src/main/java/com/clickdelivery/appmospheric/MainActivity.java
+++ app/src/main/java/com/clickdelivery/appmospheric/MainActivity.java
@@ -1,13 +1,73 @@
 package com.clickdelivery.appmospheric;
 
-import android.support.v7.app.AppCompatActivity;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import roboguice.activity.RoboActionBarActivity;
+import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
+
+/**
+ * This is the main activity where the splash screen is shown
+ */
+@ContentView(R.layout.activity_main)
+public class MainActivity extends RoboActionBarActivity {
+
+    /** Constant for animations duration **/
+    private static final int ANIM_DURATION = 1000;
+
+    /** Delay for the splash view **/
+    private static final int SPLASH_DELAY = 2500;
+
+    /** The Appmospheric Logo ImageView **/
+    @InjectView(R.id.appmospheric_logo_iv)
+    private ImageView mAppmosphericLogoIv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        fadeAnimation(mAppmosphericLogoIv);
+    }
+
+    /**
+     * This method creates a fade animation that suddenly appears "in front" of the screen to the
+     * back of the screen
+     */
+    private void fadeAnimation(View view) {
+        ObjectAnimator scaleXAnimation = createObjectAnimator(view, "scaleX", 5.0F, 1.0F);
+        ObjectAnimator scaleYAnimation = createObjectAnimator(view, "scaleY", 5.0F, 1.0F);
+        ObjectAnimator alphaAnimation = createObjectAnimator(view, "alpha", 0.0F, 1.0F);
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.play(scaleXAnimation).with(scaleYAnimation).with(alphaAnimation);
+        animatorSet.start();
+    }
+
+    /**
+     * This method creates an Object Animator based on the targeted view, the property to be
+     * animated and the initial value and final value
+     *
+     * @param view
+     *         Target view
+     * @param property
+     *         Property to be animated
+     * @param init
+     *         Initial value
+     * @param end
+     *         Final value
+     *
+     * @return ObjectAnimator with the given animated property
+     */
+    @NonNull
+    private ObjectAnimator createObjectAnimator(View view, String property, float init, float end) {
+        ObjectAnimator scaleXAnimation = ObjectAnimator.ofFloat(view, property, init, end);
+        scaleXAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
+        scaleXAnimation.setDuration(ANIM_DURATION);
+        return scaleXAnimation;
     }
 }
